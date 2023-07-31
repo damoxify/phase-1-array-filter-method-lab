ed5027a1acd6701fcb6d991f456895e33724ae15
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-31 05:06:32 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.toLowerCase() === name.toLowerCase();
+    });
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+      return driver.toLowerCase().slice(0, letters.length) === letters.toLowerCase();
+    });
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
+  
+  
+  
+  
+  
+  
